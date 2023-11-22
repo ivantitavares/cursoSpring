64a9bc533e4ea8e71ce66a1a4dd4d152f4eedae4
Ivan Tavares - TJ <ivan.tavares@tjro.jus.br>
2023-11-22 08:49:19 -0400

    Adicionando anotações do Bean Validation

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.NoArgsConstructor;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank // @NotBlank: nem nulu nem vazio para Strings
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //expressão regular \\d -> de 4 a 6 digitos
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid // para Record ser validado
+        DadosEndereco endereco) {
 
 }
